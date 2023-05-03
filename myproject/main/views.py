488f9c1fbb78cb33ed4865f0ff5178a86164027c
brysonwaisi <brysonnyamwange@gmail.com>
2023-05-03 21:52:09 +0300

    user specific page/access

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -9,23 +9,26 @@ from .forms import CreateList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
+                    print("invalid")
 
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-
-    return render(response, "main/list.html", {"ls": ls})
+        return render(response, "main/list.html", {"ls": ls})
+    return render(response, "main/view.html", {})
 # we have created custom forms without relying on django for this code
 
 
@@ -39,9 +42,15 @@ def create(response):
 
         if form.is_valid():
             n = form.cleaned_data["name"]
+            # here we are saving to db
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
         return HttpResponseRedirect("/%i" % t.id)
     else:
         form = CreateList()
     return render(response, "main/create.html", {"form": form})
+
+
+def view(response):
+    return render(response, 'main/view.html', {})
